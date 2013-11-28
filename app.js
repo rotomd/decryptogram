9996e0ae06b9a91028a282b2ae6c60ad0c056768
Michael Rotoloni <rotomd@gmail.com>
2013-11-27 23:22:56 -0500

    Algorithm for calculating a single word is now working. Refactored around this algorithm.

--- app.js
+++ app.js
@@ -9,20 +9,20 @@
         vector,
         wordlist = require("wordlist"),
         _ = require("underscore")._,
+        decrypt = require("./decrypt.js"),
         alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
         args = process.argv;
 
-    _.each(args, function(item, i){
-        if (item === "-solve" && i < args.length){
-            vector = args[i+1];
-        }
+
+    // just for testing.. will be removed later
+    var
+        inputWord = "zfmebopzobbk";
+        //inputWord = "xyyx"; 
+
+    console.log("Loading dictionary");
+    wordlist.addLanguage("en", "./wordlists/", function(){
+        console.log("Dictionary loaded");
+        console.log("getting matches for " + inputWord);
+        decrypt.parse(inputWord, wordlist);
     });
-
-    console.log(vector);
-    if (vector){
-        vector = vector.toLowerCase();
-        wordlist.init("en", function(){
-
-        });
-    }
 }());
--- decrypt.js
+++ decrypt.js
@@ -6,48 +6,65 @@
 (function(){
     "use strict";
     var
-        wordlist = require("wordlist"),
         _ = require("underscore")._,
-        //alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+        alphabetMasterList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
         factorial = _.memoize(function(i){ return (i > 1 ? i * factorial(--i) : i); }),
-        getLetters = function(inputLetters, alphabet, match, matches){
-            match = match || {};
+        counter = 0,
+        wordsFound = 0,
+        getLetters = function(alphabet, uniqVector, vector, wordlist, length, word, matches){
+            word = word || "";
+            length = isNaN(length) ? uniqVector.length : length;
             matches = matches || [];
 
-            _.times(alphabet.length, function(i){
+            _.times(length, function(index){
                 var
-                    word,
-                    letter = alphabet.splice(0, 1);
+                    itemsLeft = length - (index + 1);
 
-                if (inputLetters.length > 1){
-                    match[inputLetters.splice(0,1)] = letter;
-                    getLetters(inputLetters, alphabet, match, matches);
-                }else{
-                    match[inputLetters[0]] = letter;
-                    matches.push(match);
-                }
+                _.times(alphabet.length, function(abcIndex){
+                    var
+                        match, testWord,
+                        alpha = _.clone(alphabet),
+                        currentWord = _.clone(word);
+
+                    currentWord += alpha.splice(abcIndex, 1)[0];
+                    if (currentWord.length === uniqVector.length){
+                        counter++;
+                        match = {};
+                        testWord = vector;
+                        _.each(uniqVector, function(letter, i){
+                            var
+                                regEx = new RegExp(letter, "g");
+
+                            match[letter] = currentWord[i];
+                            testWord = testWord.replace(regEx, currentWord[i]);
+                        });
+                        match.word = testWord;
+                        console.log(currentWord + ", " + testWord);
+                        if (wordlist.checkWord("en", testWord)){
+                            console.log("Valid Word > " + testWord);
+                            matches.push(match);
+                            wordsFound++;
+                        }
+                    }
+                    getLetters(alpha, uniqVector, vector, wordlist, itemsLeft, currentWord, matches);
+                });
             });
 
             return matches;
         },
-        parse = function(word){
+        parse = function(word, wordlist){
             var
-                inputLetters = _.uniq(s),
-                combinations = factorial(alphabet.length) / factorial(alphabet.length - inputLetters.length),
-                matches = [];
+                inputLetters = _.uniq(word),
+                combinations = factorial(alphabetMasterList.length) / factorial(alphabetMasterList.length - inputLetters.length),
+                matches;
 
-            console.log("Parsing " + word + ", " + combinations " total combinations");
-            wordlist.init("en", function(){
-                var
+            console.log("Parsing " + word + ", " + combinations + " total combinations");
+            matches = getLetters(alphabetMasterList, inputLetters, word, wordlist);
+            console.log("Created " + counter + " combinations, found " + wordsFound + " valid words");
+            console.log("matches: ");
+            console.log(matches);
 
-                    location = 0;
-
-                _.times(26, function(rootIndex){
-                    var
-                        alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-                        letterPicks = {};
-                });
-            });
+            return matches;
         };
 
     exports.parse = parse;
